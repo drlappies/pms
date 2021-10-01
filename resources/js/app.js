7e84f45c3bc86326de1f1e4fd8ddb6461b514ebb
drlappies <yau1chun2lap3@gmail.com>
2021-10-01 23:26:35 +0800

    vue js scaffolding

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+window.Vue = require('vue').default;
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+import App from './components/App.vue'
+
+const app = new Vue({
+    el: '#app',
+    components: {
+        App
+    }
+});
---
+++ resources/js/components/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>APP COMPONENT</div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
